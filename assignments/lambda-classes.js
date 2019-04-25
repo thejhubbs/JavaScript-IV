bc7dbcf11a763083f7b8f27469310ba08b1aac22
Jordan Hubbard <thejhubbs@gmail.com>
2019-04-25 19:24:48 -0400

    added class skeletons to lambda-classes.js

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,31 @@
 // CODE here for your Lambda Classes
+class Person {
+    constructor(attr){
+
+    }
+}
+
+
+class Student extends Person {
+    constructor(attr){
+        super(attr);
+    }
+}
+
+
+
+class Instructor extends Person {
+    constructor(attr){
+        super(attr);
+    }
+}
+
+
+
+class ProjectManager extends Instructor {
+    constructor(attr){
+        super(attr);
+    }
+}
+
+
